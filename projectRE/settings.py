171b08875d66672eb08d736056160970be593adf
Serhiy Yarosh <m7grey@gmail.com>
2019-08-27 16:21:41 -0400

    Add Messages.

--- projectRE/settings.py
+++ projectRE/settings.py
@@ -129,3 +129,12 @@ STATICFILES_DIRS = [
 # Media Folder Setting
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#Messages
+from django.contrib.messages import get_messages
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.INFO: '',
+    50: 'critical',
+}
