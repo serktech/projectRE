7567cc53d117ebacac61d79d9d3a2a347ecdb911
Serhiy Yarosh <m7grey@gmail.com>
2019-08-28 14:34:01 -0400

    Production Functionality- credentials.

--- projectRE/settings.py
+++ projectRE/settings.py
@@ -142,13 +142,6 @@ MESSAGE_TAGS = {
 
 # Email config
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'your_account @ gmail.com'
-# EMAIL_HOST_PASSWORD = 'youraccount’spassword'
-# EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
